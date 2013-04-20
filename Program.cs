96fdcbe36fd7d8cc27980a6b47f3e8f8f357bd17
tomekl007 <tomekl007@gmail.com>
2013-04-20 11:18:52 -0700

    TO DO : miltithreadeing

--- Program.cs
+++ Program.cs
@@ -27,66 +27,15 @@ namespace ParsXtmlExamle
             stocks.Add("NASDAQ");
             stocks.Add("AMEX");
 
+            // * is placeholder to replace by stock and ^ for letter
+            String baseUrl = "http://www.findata.co.nz/markets/*/symbols/^";
 
-            WebClient webClient = new WebClient();
-            String filePath = @"\temp.html";
-            webClient.DownloadFile("http://www.findata.co.nz/markets/NYSE/symbols/Q.htm", filePath);
+            Extractor ex = new Extractor(baseUrl, stocks[0], alphabet[22],".htm");
+            ex.Extract();
 
          
 
-            HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
-
-            // There are various options, set as needed
-            htmlDoc.OptionFixNestedTags = true;
-
-            // filePath is a path to a file containing the html
-            htmlDoc.Load(filePath);
-          //  Console.WriteLine("" + htmlDoc.DocumentNode.SelectSingleNode("//body").InnerText);
-
-            // Use:  htmlDoc.LoadHtml(xmlString);  to load from a string (was htmlDoc.LoadXML(xmlString)
-
-            // ParseErrors is an ArrayList containing any errors from the Load statement
-            if (htmlDoc.ParseErrors != null && htmlDoc.ParseErrors.Count() > 0)
-            {
-                Console.WriteLine("paresErrors != null" );
-                foreach(var s  in htmlDoc.ParseErrors){
-                    Console.WriteLine(s.Reason);
-                }
-                
-
-            }
-          
-              
-            FileHelper fileHelper = new FileHelper("NYSE","Q");
-            //tr
-            Console.WriteLine("TR----------------");
-            HtmlNodeCollection allTr = htmlDoc.DocumentNode.SelectNodes("//tr");
-            //HtmlNodeCollection allTd = htmlDoc.DocumentNode.SelectNodes("//tr//td");
-            //Console.WriteLine("after reg-------->");
-            //foreach (HtmlNode n in allTd)
-              //  Console.WriteLine(n.InnerText);
-
-            
-           // bool firstIteration = true;//first element not put to file
-            int i = 0;
-            foreach (HtmlNode node in allTr)
-            {
-
-                if (i>=2)
-                {
-                    //Console.WriteLine(node.InnerText);
-                   
-
-                    HtmlNodeCollection allTdForTr = node.ChildNodes;
-                    fileHelper.saveLineToFile( allTdForTr);
-                }
-                if (i < 2)
-                    i++;
-
-            }
-
-            fileHelper.closeFile();
-  
+           
 
            
 
